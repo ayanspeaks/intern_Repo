e87b5e9eccfd5d1a9b6bbb818a741428d3c09c5b
ayanspeaks <ayans.2003kali@gmail.com>
2025-03-12 12:47:59 +0530

    spring jdbc updated

--- Java5/SpringJDBC/src/main/java/com/test/dao/CustDao.java
+++ Java5/SpringJDBC/src/main/java/com/test/dao/CustDao.java
@@ -1,33 +1,66 @@
 package com.test.dao;
 
+import java.sql.ResultSet;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import com.test.model.Customer;
 
 public class CustDao {
 	
-	public static void main(String[] args) {
 	@Autowired
-	public JdbcTemplate() {
-		return jdbcTemplate;
-	}
-
+	private JdbcTemplate jdbcTemplate;
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	private JdbcTemplate jdbcTemplate;
-	
-	public int createCustomer(Customer cst) {
-		String sql = "insert into customer1 (cst_name, cst_type) values('"+cst.getName()+"', '"+cst.getType()+"')";
+	public void createCustomer(Customer cst) {
+		String sql = "insert into customer(cst_name, cst_type) values('"+cst.getName()+"', '"+cst.getType()+"')";
 		
-	     jdbcTemplate.execute(sql);
+		jdbcTemplate.execute(sql);
 	}
 	
-	public int deleteCustomer(Customer cst) {
-		
+	public void updateCustomer(Customer cst) {
+		String sql = "update customer set cst_name = '"+cst.getName()+"', cst_type = '"+cst.getType()
+				+ "' WHERE id =" + cst.getId();
+		jdbcTemplate.execute(sql);
 	}
+	
+	//read customer data
+	
+	public List<Customer> getAllCustomers(){
+		return jdbcTemplate.query("select * from customer1", new ResultSetExtractor<List<Customer>>() {
+			{
+				@Override
+				public List<Customer> extractData(ResultSet rs) throws SQLException,
+				DataAccessException{
+					
+					List<Customer> listData = new ArrayList<>();
+					
+					while(rs.next()) {
+						Customer obj = new Customer();
+						
+						obj.setId(rs.getInt(1));
+						obj.setName(rs.getString(2));
+						obj.setType(rs.getString(3));
+						
+						listData.add(obj);
+					}
+					
+					List<Customer> data = cst.getAllCustmers();
+					
+					for(Customer cstd : data) {
+						System.out.println(cstd);
+					}
+					return listData;
+				}
+			}
+		});
 	}
+	
 	
 }
--- Java5/SpringJDBC/src/main/java/com/test/main/StoreData.java
+++ Java5/SpringJDBC/src/main/java/com/test/main/StoreData.java
@@ -7,18 +7,28 @@ import com.test.dao.CustDao;
 import com.test.model.Customer;
 
 public class StoreData {
-	
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 		
 		Customer obj = new Customer();
-		
-		obj.setName("Ayan");
-		obj.setType("Regular");
+		obj.setName("Virat");
+		obj.setType("Preferred");
 		
 		CustDao cst = ctx.getBean("custDao", CustDao.class);
-				
-		cst.createCustomer(obj);
+//		cst.createCustomer(obj);
+		
+		
+		Customer objUpdate = new Customer();
+		obj.setName("Rahul");
+		obj.setType("visiting");
+		obj.setId(2);
+		cst.updateCustomer(objUpdate);
+		
+		System.out.println("done updating in db");
+		
+		cst.updateCustomer(obj);
+		
+		System.out.println("Done.");
+		
 	}
-
 }
