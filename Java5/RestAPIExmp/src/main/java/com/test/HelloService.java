9ef3d522ccb3972b6c826734465773942166c1e4
ayanspeaks <ayans.2003kali@gmail.com>
2025-03-13 12:43:00 +0530

    api

---
+++ Java5/RestAPIExmp/src/main/java/com/test/HelloService.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloService {
+	
+	@GET
+	@Path("/text")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello() {
+		return "This is imple RestAPI plain text response";
+	}
+
+}
---
+++ Java5/RestAPIExmp/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<h2>Simple RestAPI Example</h2>
+
+	<a href="rest/hello/text">click here</a>
+
+</body>
+</html>
