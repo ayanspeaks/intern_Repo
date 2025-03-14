5b4b1bf148c0f3c1b3830d467a0e8f99763a5371
ayanspeaks <ayans.2003kali@gmail.com>
2025-03-14 12:56:17 +0530

    api second updated postman

---
+++ Java5/SpringBootRestAPIJPA/src/main/java/com/test/dao/impl/CustomerDAOImpl.java
@@ -0,0 +1,49 @@
+package com.test.dao.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.test.dao.CustomerDao;
+import com.test.entity.Customer;
+import com.test.repository.CustomerRepository;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDao{
+
+	@Autowired
+	private CustomerRepository repository;
+	
+	
+	@Override
+	public Customer createCustomer(Customer cst) {
+		// TODO Auto-generated method stub
+		return repository.save(cst);
+	}
+
+	@Override
+	public List<Customer> readAllCustomers() {
+		// TODO Auto-generated method stub
+		return repository.findAll();
+	}
+
+	@Override
+	public List<Customer> updateCustomer(Customer cst) {
+		// TODO Auto-generated method stub
+		repository.save(cst);
+		
+		return repository.findAll();
+	}
+
+	@Override
+	public List<Customer> deleteCustomer(int id) {
+		// TODO Auto-generated method stub
+		repository.deleteById(id);
+		
+		return repository.findAll();
+	}
+	
+	
+
+}
