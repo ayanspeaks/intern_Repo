57f3b1661228ee4d99d9d5d47f18004e74cc73a1
ayanspeaks <ayans.2003kali@gmail.com>
2025-03-18 12:25:47 +0530

    cloud config

---
+++ Java5/SpringBootRestAPIS1/src/main/java/com/test/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	
+	@GetMapping("/service1")
+	public String productService() {
+		return "Simple Product Controller Response";
+	}
+
+}
