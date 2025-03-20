c2b7e5811e66e07670401fa8e6255d6b928d7d5d
ayanspeaks <ayans.2003kali@gmail.com>
2025-03-20 15:08:54 +0530

    httpclient

--- angular/angular-httpclient-app/src/app/app.component.ts
+++ angular/angular-httpclient-app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-form-validation';
+  title = 'angular-httpclient-app';
 }
